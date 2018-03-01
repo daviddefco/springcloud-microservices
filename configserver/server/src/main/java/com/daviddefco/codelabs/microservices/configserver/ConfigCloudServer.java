8e88c16a309211f8e7b7b118c2261be2a8f51fd8
daviddefco <david.de.fco@gmail.com>
2018-03-01 11:28:26 +0100

    Added spring cloud config server sample code

---
+++ configserver/server/src/main/java/com/daviddefco/codelabs/microservices/configserver/ConfigCloudServer.java
@@ -0,0 +1,14 @@
+package com.daviddefco.codelabs.microservices.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCloudServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCloudServer.class, args);
+    }
+}
